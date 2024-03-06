9b7857fda621017bf4d76dcb21410fe881ecf5a9
Owen G <144268700+OwenGirard@users.noreply.github.com>
2024-03-06 10:33:30 -0800

    f

--- Assets/Owen's stuff/Damages.cs
+++ Assets/Owen's stuff/Damages.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Damages : MonoBehaviour
 {
+    public static CardHud hud;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,15 @@ public class Damages : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       // if (("combat"))
+      //  {
+      //      ChangeHealth(-1);
+      //  }
         
+      //   if ("health")) 
+      //  {
+      //      ChangeHealth( +1);
+      //      other.gameObject.SetActive(false);
+      
     }
 }
