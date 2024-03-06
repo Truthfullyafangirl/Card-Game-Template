a78d0a3e44333a9759581c8cb01079b1e32ef71a
Owen G <144268700+OwenGirard@users.noreply.github.com>
2024-03-06 11:30:00 -0800

    f

--- Assets/Owen's stuff/Damages.cs
+++ Assets/Owen's stuff/Damages.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Damages : MonoBehaviour
@@ -7,34 +8,48 @@ public class Damages : MonoBehaviour
 {
     public static CardHud hud;
     public int health;
+
+    public bool combat;
     // Start is called before the first frame update
     void Start()
     {
-        
+        combat = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       // if (("combat"))
-      //  {
-      //      ChangeHealth(-1);
-      //  }
+        //if(sheild card is clicked )
+        {
+            combat = false;
+        }
+        //else if (sword card is clicked)
+        {
+            combat = true;
+        }
+        //if (combat = true || end of turn )
+        {
+     //       health(-1);
+        }
+      //  if ("healthcard"))
+        {
+      //      health(+1);
+        }
+        if (health <=0 )
+        {
+            // Switch to the next scene
+            SceneManager.LoadScene("Title screen");
+        }
+
+    }
         
-      //   if ("health")) 
-      //  {
-      //      ChangeHealth( +1);
-      //      other.gameObject.SetActive(false);
-      
     
-      if (health <=0 )
-      {
-          // Switch to the next scene
-          SceneManager.LoadScene("Title screen");
-      }
-    
+
+
+
+
     
     
     }
-}
+
 
